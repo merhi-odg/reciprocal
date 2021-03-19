14e7fde10a053578f5d03090043446509ca3c4ae
merhi-odg <51339977+merhi-odg@users.noreply.github.com>
2021-03-19 15:55:20 -0500

    Update code.py

--- code.py
+++ code.py
@@ -12,8 +12,17 @@ def action(data):
     param: data: dict of he form {"input": x} for some number x
     """
     
+    print("action input: ", data, flush=True)
+    
     num = data["input"]
     
-    yield {
+    print("num: ", num, flush=True)
+    
+    out = {
         "reciprocal": 1/num
     }
+    
+    print("output: ", out, flush=True)
+
+
+    yield out
