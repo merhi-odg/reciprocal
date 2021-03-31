1313fb9f6445ea89bb7bf5c8ef0d8316c13b6a0d
merhi-odg <51339977+merhi-odg@users.noreply.github.com>
2021-03-31 16:27:25 -0500

    rollback changes

--- code.py
+++ code.py
@@ -2,32 +2,8 @@
 # modelop.slot.1: in-use
 
 
-import json
-
-
 # modelop.init
-def begin(model_def):
-    
-    global model_definition
-    global input_schema_definition
-    
-    print("\nModel Definition keys: \n", flush=True)
-    print(model_def.keys(), flush=True)
-    
-    if 'job' in model_def.keys():
-        print(model_def['job'], flush=True)
-    
-    elif 'deployedModel' in model_def.keys():
-        print(model_def['deployedModel'], flush=True)
-    
-    model_definition = model_def
-    
-    input_schema = json.loads(
-        model_def['rawJson']
-    )['model']['storedModel']['modelMetaData']['inputSchema'][0]
-    
-    input_schema_definition = input_schema['schemaDefinition']
-    
+def begin():
     pass
 
 
@@ -49,7 +25,4 @@ def action(data):
     
     print("output: ", out, flush=True)
     
-    yield input_schema_definition
-    
-    # yield model_definition
-    # yield out
+    yield out
