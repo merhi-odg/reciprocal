287116c59fa9ea56bf359611f8854bde6157da82
merhi-odg <51339977+merhi-odg@users.noreply.github.com>
2021-03-31 15:56:52 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,10 +2,14 @@
 # modelop.slot.1: in-use
 
 
+import json
+
+
 # modelop.init
 def begin(model_def):
     
     global model_definition
+    global input_schema_definition
     
     print("\nModel Definition keys: \n", flush=True)
     print(model_def.keys(), flush=True)
@@ -18,7 +22,14 @@ def begin(model_def):
     
     model_definition = model_def
     
+    input_schema = json.loads(
+        model_def['rawJson']
+    )['model']['storedModel']['modelMetaData']['inputSchema'][0]
+    
+    input_schema_definition = input_schema['schemaDefinition']
+    
     pass
+
 
 # modelop.score
 def action(data):
@@ -37,6 +48,8 @@ def action(data):
     }
     
     print("output: ", out, flush=True)
-
-    yield model_definition
+    
+    yield input_schema_definition
+    
+    # yield model_definition
     # yield out
