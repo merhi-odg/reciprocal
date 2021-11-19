4edb5a5252079088e6129a0b694d726f03131e36
Sami Merhi <51339977+merhi-odg@users.noreply.github.com>
2021-11-19 14:10:29 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,38 +1,43 @@
 # modelop.slot.0: in-use
 # modelop.slot.1: in-use
 
-
 import time
+import logging
+import warnings
+
+# Line below is not needed since engine's jet.py set it globally
+# warnings.filterwarnings("error", category=UserWarning) # Coerce UserWarnings into erros
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level="INFO")
+
 
 # modelop.init
-def begin():
+def begin() -> None:
     
-    print("\nWaiting 15s before erroring out init function\n", flush=True)
-    
-    time.sleep(15)
-    
-    print("\nErroring out init on purpose. Model should not get deployed!\n", flush=True)
+    logging.info("Waiting 10s before erroring out init function")
+    time.sleep(10)
+    logging.info("Erroring out init on purpose. Model should not get deployed!")
     print(1/0)
     
     pass
 
+
 # modelop.score
-def action(data):
+def action(data: float) -> dict:
     """
-    param: data: dict of he form {"input": x} for some number x
+    param: data: dict of the form {"input": x} for some number x
     """
     
-    print("action input: ", data, flush=True)
-    
-    num = data["input"]
-    
-    print("num: ", num, flush=True)
-    
-    out = {
-        "reciprocal": 1/num
-    }
-    
-    print("output: ", out, flush=True)
+    logger.info("Input to action(): %s", data)
 
+    if not isinstance(data, dict):
+        raise UserWarning("Bad Input: input data should be a dictionary")
+    if not "input" in data:
+        warnings.warn("Key 'input' missing from input data", category=UserWarning)
+    if data["input"]==0:    
+        raise UserWarning("Bad Input: DivisionByZero Expected!")
+    
+    output = {"reciprocal": 1/data["input"]}
 
-    yield out
+    yield output
